40f52e9651cb9a130ee76be1a9a195de1e7045e1
ankushdharkar <ankushdharkar@gmail.com>
2025-01-11 14:21:37 -0500

    added testing via autograder

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# pnpm-template
-A template to create other assignments
+# Bank Transactions
+
+Write class for: 
+- Creating a bank
+- Creating a bank account in the bank
+- Sending money from one account to another
+- Checking the balance of an account
+- Checking the transaction history of an account
--- src/__tests__/real.test.ts
+++ src/__tests__/real.test.ts
@@ -2,11 +2,14 @@ import { describe, it, expect } from 'vitest';
 import isEqualChecker from '@/index';
 
 describe('verify answers', () => {
-  it('should equate same objects', () => {
-    expect(isEqualChecker({ b: 2, a: 1 }, { a: 1, b: 2 })).toBe(true);
+  it('should exist', () => {
+    return true;
   });
+  // it('should equate same objects', () => {
+  //   expect(isEqualChecker({ b: 2, a: 1 }, { a: 1, b: 2 })).toBe(true);
+  // });
 
-  it('should not equate different  objects', () => {
-    expect(isEqualChecker({ a: 1, c: 2 }, { a: 1, b: 2 })).toBe(false);
-  });
+  // it('should not equate different  objects', () => {
+  //   expect(isEqualChecker({ a: 1, c: 2 }, { a: 1, b: 2 })).toBe(false);
+  // });
 });
